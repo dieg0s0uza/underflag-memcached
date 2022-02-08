4edde693cb79782945baf8098031d5c0e4519dcf
Diego Souza <diego.souza@i-value.com.br>
2022-02-08 02:21:50 -0300

    update underflag version

--- src/providers/MemcachedCacheProvider.ts
+++ src/providers/MemcachedCacheProvider.ts
@@ -1,4 +1,4 @@
-import { ICacheProvider, CacheModel } from 'underflag';
+import { ICacheProvider, DataModel } from 'underflag';
 
 interface Options {
     /** A client instance to Memcached */
@@ -14,11 +14,11 @@ export class MemcachedCacheProvider implements ICacheProvider {
         this.client = options.client;
         this.lifetime = options.lifetime || 3600;
     }
-    async get(key: string): Promise<CacheModel | undefined> {
+    async get(key: string): Promise<DataModel | undefined> {
         const { value } = await this.client.get(key);
         return value ? { key, value: JSON.parse(value) } : undefined;
     }
-    async set(data: CacheModel): Promise<void> {
+    async set(data: DataModel): Promise<void> {
         await this.client.set(data.key, JSON.stringify(data.value), { expires: this.lifetime });
     }
 }
